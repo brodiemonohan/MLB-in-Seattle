288a8839e88929a0ede433504de4255d2923cac6
Brodie Monohan <brodiemonohan@Brodies-MacBook.local>
2026-07-25 17:03:13 -0700

    Updated README. Updated c_d_baseball function and trajectory function. Both now work.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# T-Mobile-Park-s-Effect-On-Major-League-Baseball-in-Seattle
+# T-Mobile Park's Effect On Major League Baseball in Seattle
+
+The three forces that determine the path of a baseball though the air are gravity, drag, and the Magnus force. This project simulates these forces on a batted baseball (given parameters like velocity and spin rate) for a given MLB stadium using a combination of lattitude, longitude, and elevation data scraped from Google Maps, as well as historical weather data scraped from the Open Mateo libraries. It is important to note that the Magnus forces is not strictly lift as it points perpendicular to the direction of motion and the spin axis, typically making a lift force only a component of the overall force and effect.
+
+## flight_trajectory_numerical_calculation.py
+
+This file defines 4 helper functions that compute the drag and Magnus effect coefficents due to drag and the Magnus force, as well as the forces themselves based on the required input parameters. The 5th function combines these helper functions with the force due to gravity into a comprehensive function that numerically computes the flight path of the ball in the 2D x-z plane (keeing the same axis convention as StatCast) by computing the forces, the velocity, and position of the ball at time steps defined by the user.
--- T-Mobile-Park-s-Effect-On-Major-League-Baseball-in-Seattle/flight_trajectory_numerical_calculation.py
+++ T-Mobile-Park-s-Effect-On-Major-League-Baseball-in-Seattle/flight_trajectory_numerical_calculation.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 def c_l_baseball(v: float, omega: float) -> float:
     '''
-    Takes in an initial velocity (v, m/s) and angular velocity (omega, rad/s) for a baseball 
-    and returns the lift coefficent (C_l) which is generated due to the magnus 
+    Takes in an initial velocity (v, m/s) and angular velocity (omega, rad/s) for a 
+    baseball and returns the lift coefficent (C_l) which is generated due to the magnus 
     effect as parameterized by Sawicki, Hubbard, and Stronge (2003).
     '''
     R = 0.0364 #radius of MLB baseball (Nathan 2007)
@@ -19,11 +19,11 @@ def c_l_baseball(v: float, omega: float) -> float:
 def f_m_baseball(v: float, omega: float, rho: float, axis: float = 0) -> float:
     '''
     Takes an initial velocity (v, m/s), an angular velocity (omega, rad/s), air density
-    (rho, kg/m^3), and the axis of rotation (axis, rad), and, in conjunction with the c_l_baseball function, returns the force
-    due to the magnus effect.
+    (rho, kg/m^3), and the axis of rotation (axis, rad), and, in conjunction with the 
+    c_l_baseball function, returns the force due to the magnus effect.
     '''
     A = np.pi * (0.0364 ** 2) #cross-sectional area of an MLB baseball (Nathan 2007)
-    return (1 / 2) * c_l_baseball(v, omega) * rho * A * (v ** 2)
+    return ((1 / 2) * c_l_baseball(v, omega) * rho * A * (v ** 2)) * np.cos(axis)
 
 
 def c_d_baseball(v: float, omega: float) -> float:
@@ -47,43 +47,75 @@ def f_d_baseball(v: float, omega: float, rho: float) -> float:
     return (1 / 2) * c_d_baseball(v, omega) * rho * A * (v ** 2)
 
 
-def trajectory(v: float, omega: float, rho: float, angle: float, axis: float, step: float = 0.01) -> None:
+def trajectory(v: float, omega: float, rho: float, angle: float, axis: float = 0, 
+                step: float = 0.01, label: str = None, color: str = 'k', marker: str = None,
+                linestyle: str = None, g: float = 9.81, m: float = 0.145, x: float = 0,
+                z: float = 1, show_distance: bool = False, label_density: bool = True, 
+                savefig: bool = False) -> None:
     '''
-    Takes advanced statistics exit velocity (v, m/s), spin rate (omega, rad/s), air density 
-    (rho, kg/m^3), launch angle (angle, rad), spin vector (axis, rad), and a time step (step, s) 
-    and produces a numerically calculated trajectory of an MLB baseball via plotting the 2D position
-    of the ball in x-z space while z is positive.
+    Takes required arguments exit velocity (v, m/s), spin rate (omega, rad/s), air density
+    (rho, kg/m^3), launch angle (angle, rad), and spin vector (axis, rad), as well as default
+    arguments time step (step, s), trajectory label, line color, step marker, line style,
+    acceleration due to gravity (g), the mass of the ball (m) the initial position of the 
+    ball (x), and the initial height of the ball (z). The function produces a numerically 
+    calculated trajectory of an MLB baseball via plotting the 2D position of the ball in x-z 
+    space while z is positive and returns the final x distance. The savefig function is 
+    not called so that multiple instances of the trajectory function can be stacked in a 
+    single graph to show parameter variation.
     '''
-    # inital values metric
-    m = 0.145 # mass of MLB baseball (Nathan 2007) (kg)
-    g = 9.81 # acceleration due to gravity (m/s/s)
+    
+    # inital values
     v_x = np.cos(angle) * v
     v_z = np.sin(angle) * v
-    x = 0 # initial position at home plate
-    z = 1 # initial height 1 meter above home plate
 
-    # while z > 0:
-    for i in range(100):
+    # initialize x and z lists
+    xs = [x]
+    zs = [z]
+    
+    while z > 0:
         
         f_x = (-1 * (f_d_baseball(v, omega, rho) * np.cos(angle))
-               - (f_m_baseball(v, omega, rho, axis)) * np.sin(angle))
+                - (f_m_baseball(v, omega, rho, axis)) * np.sin(angle))
         
         f_z = (-1 * (f_d_baseball(v, omega, rho) * np.sin(angle))
                + (f_m_baseball(v, omega, rho, axis) * np.cos(angle))
-              - m * g)
+               - m * g)
 
         # update for this time step
         v_x += (f_x / m) * step
         v_z += (f_z / m) * step
         x += v_x * step
         z += v_z * step
+        xs.append(x)
+        zs.append(z)
 
         # update for next time step calculation
         v = np.sqrt((v_x ** 2) + (v_z ** 2))
         angle = np.arctan(v_z / v_x)
+
+    # plotting
+    if label_density and g == 9.81:
+        plot_label = f'{label} ({rho} $\\rm kg/m^3$)'
+    elif label_density:
+        plot_label = f'{label} ({rho} $\\rm kg/m^3$, g = {g} $\\rm m/s^2$)'
+    else:
+        plot_label = label
         
-        #plot
-        plt.plot(x, z, color = 'k')
+    if show_distance:
+        plt.axvline(xs[-1], color = color, linestyle = ':')
+    
+    if marker:
+        plt.scatter(xs, zs, color = color, marker = marker)
+        
+    plt.plot(xs, zs, color = color, label = plot_label, linestyle = linestyle)
     plt.ylabel('Height (m)')
     plt.xlabel('Distance (m)')
-    
+    plt.legend()
+
+    if savefig and label:
+        plt.savefig(label + '_trajectory_plot.png', bbox_inches = 'tight')
+    elif savefig:
+        plt.savefig('trajectory_plot.png', bbox_inches = 'tight')
+
+    # return final distance
+    return xs[-1]
